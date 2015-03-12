e2abbab6a57e1e0bdd60e36bcccd9edd90af12cc
Marte Jørgensen <martejorgensen@Martes-MacBook-Pro.local>
2015-03-12 19:27:25 +0100

    done someting whit a template or something

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,4 +6,8 @@ from plls import views
 urlpatterns = patterns('',
 	url(r'^polls/', include('polls.urls'))
 	url(r'^admin/', include(admin.site.urls))
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+	url(r'^(?P<question_id>/d+)/vote/$', views.vote, name='vote'),
 )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from polls.models import Questions
+
 
 # Create your views here.
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {
+		'latest_question_list': latest_question_list,
+	})
+	return HttpResponse(template.render(context))
+
+def detail(request, question_id):
+	return HttpResponse("Your're looking at question %s." % question_id)
+
+def result(request, question_id):
+	response = "You're looking at the result og question %s."
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
